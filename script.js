d75c2ae3fc18e811fac5af8917b4e15760340a7f
Kenny Yang <kennyyangh@gmail.com>
2020-07-25 12:50:42 -0700

    added files for html css js and made notes

---
+++ script.js
@@ -0,0 +1,13 @@
+// when we click start, timer will start and question 1 will appear
+// question 1 will have 4 choices, either you get the answer right or wrong
+//              what happens when wrong?
+//                      you deduct from the timer
+//                      log in their score as 1 wrong (maybe might not need to do this)
+//                      then we move onto the next question
+//              what heppens whenr ight?
+//                  log in their score as 1 right
+//                  move onto the next quesiton
+//once timer or questions are taken up, we will set the page to game over and have their score listed onto the page
+//we ask user to input initials and we save up in the local storage where user can access their score at a later time on the top left of the page
+
+//might need to add a 'go back' function when looking at scores
